73e51cd74bc17d9914609eef1cdcf630fbbdd39f
SushanthSuvarna <sushanths5454@gmail.com>
2024-05-12 15:25:40 +0530

    Initial commit

---
+++ News/News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Sushanth on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
